840b7719a5fd558c04099d48a47aa411a3529126
douningbo <douningbo@aiforward.com>
2020-02-24 04:31:22 +0800

    update

--- model.go
+++ model.go
@@ -25,6 +25,20 @@ type MinipOrderRequest struct {
 }
 
 //小程序订单-返回
+/**
+ * 返回信息:
+ * return_code          return_msg
+ * --------------------------------------
+ * ORDER_NOT_EXIST      订单不存在
+ * ORDER_MISMATCH       订单号与商户不匹配
+ * SYSTEMERROR          系统内部异常
+ * INVALID_SHORT_ID     商户编码不合法或没有对应商户
+ * SIGN_TIMEOUT         签名超时，time字段与服务器时间相差超过5分钟
+ * INVALID_SIGN         签名错误
+ * PARAM_INVALID        参数不符合要求，具体细节可参考return_msg字段
+ * NOT_PERMITTED        未开通网关支付权限
+ * --------------------------------------
+ */
 type MinipOrderResponse struct {
 	ReturnCode string `json:"return_code"` //执行结果
 
